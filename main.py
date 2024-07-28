ce18e961101fb039d6f46dfc67a4f5e3bb28bcbe
Tumi Jourdan <2180153@students.wits.ac.za>
2024-07-28 21:37:38 +0200

    Results capture

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import numpy as np
+import matplotlib.pyplot as plt
+
 
 class Arm:
     def __init__(self):
@@ -23,6 +25,24 @@ class MultiArmedBandit:
             return self.arms[arm_index].pull()
         else:
             raise ValueError("Invalid arm index.")
+    
+class Results:
+    def __init__(self,num_arms):
+        self.arms = num_arms
+        self.results = []
+        
+    def store_results(self,result):
+        self.results.append(result)
+    
+    def displayGraphs(self):
+        steps = self.results.count
+        averages = []
+        for x in self.results:
+            averages.append(np.mean(x))
+        plt.subplot(1, 2, 1)  # 1 row, 2 columns, 1st subplot
+        plt.xticks(range(len(averages)))
+        
+
 
 def main():
     num_arms = 10  # Number of arms in the bandit
@@ -33,10 +53,31 @@ def main():
         reward = mab.pull_arm(i)
         print(f"Pulled arm {i}, received reward: {reward:.2f}")
 
+    
 
-def epsilon_greedy_optimistic():
-    #pass
-    print("fuck u python")
+
+def epsilon_greedy_optimistic(mab:MultiArmedBandit,iterations:int,alpha:float, optimistic_estimate:float):
+    # need ten running sums
+    running_sums = np.zeros(10)
+    
+    for x in running_sums:
+        x = optimistic_estimate
+    # Explore or exploit
+    
+    # exploit
+    max_estimate = np.max(running_sums)
+    candidates = np.where(running_sums == max_estimate)[0]
+    arm_index =  np.random.choice(candidates)
+    # Explore
+    arm_index = np.random.choice(running_sums)
+    
+    # do action
+    reward = mab.pull_arm(arm_index)
+    
+    Qn = running_sums[arm_index]
+    Rn = reward
+    running_sums[arm_index] +=Qn + alpha*(Rn - Qn)
+    
 
 if __name__ == "__main__":
     main()
