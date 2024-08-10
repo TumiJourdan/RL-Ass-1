70e2d4bdfefe4f119f58f52459724dbb91951622
Tumi Jourdan <2180153@students.wits.ac.za>
2024-08-10 12:08:12 +0200

    Final Push

--- Lab 2/main.py
+++ Lab 2/main.py
@@ -5,10 +5,10 @@ import matplotlib.pyplot as plt
 
 
 
-def debugBoards(randAgent:RandomAgent,smartAgent:SmartAgent):
-    path = randAgent.getPath()
+def debugBoards(agent):
+    path = agent.getPath()
     
-    x,y = zip(*randAgent.getPath())
+    y,x = zip(*agent.getPath())
     plt.scatter(x,y,color='blue', marker='o')
     for i in range(len(x) - 1):
         plt.annotate(
@@ -17,6 +17,11 @@ def debugBoards(randAgent:RandomAgent,smartAgent:SmartAgent):
             xytext=(x[i], y[i]), 
             arrowprops=dict(arrowstyle='->', color='red', lw=1.5)
         )
+    plt.xlabel('X-axis')
+    plt.ylabel('Y-axis')    
+    plt.xlim(0, 6)
+    plt.ylim(0, 6)
+    plt.gca().invert_yaxis()
     plt.show()
 
 START = (6,0)
@@ -30,25 +35,39 @@ policy=grid.gridWorld
 board = world.getBoard()
 print(board)
 
-agent1 = RandomAgent(position=START)
-agent2 = SmartAgent(position=START)
+def run():
+    agent1 = RandomAgent(position=START)
+    agent2 = SmartAgent(position=START)
+    world = World((obstacleList), START, GOAL)
+    # ================== AGENT 1 ====================
+    print(f"AgentOne initial position: {agent1.position}")
+    for _ in range(50):
+        move1 = agent1.chooseMove(movelist)
+        agent1.makeMove(move1, world)
+        
+    print(f"AgentOne final position: {agent1.position}")
+    print(f"AgentOne world final position: {world.getPath()[-1]}")
 
-# ================== AGENT 1 ====================
-print(f"AgentOne initial position: {agent1.position}")
-for _ in range(50):
-    move1 = agent1.chooseMove(movelist)
-    agent1.makeMove(move1, world)
+    # ================== AGENT 2 ====================
+    world = World((obstacleList), START, GOAL)
+    print(f"Agent Two initial position: {agent2.position}")
+    while agent2.currentReward!=20:
+        move1 = agent2.chooseMove(movelist,policy)
+        agent2.makeMove(move1, world)
+    print(f"AgentTwo final position: {agent2.position}")
+    return agent1,agent2
+
+def many_runs(runs:int):
+    sum1 = 0
+    sum2 = 0
+    for i in range(runs):
+        agent1,agent2 = run()
+        sum1 +=agent1.reward
+        sum2 += agent2.reward
+    return sum1/runs,sum2/runs
     
-print(f"AgentOne final position: {agent1.position}")
-print(f"AgentOne world final position: {world.getPath()[-1]}")
 
-# ================== AGENT 2 ====================
-world = World((obstacleList), START, GOAL)
-print(f"Agent Two initial position: {agent2.position}")
-while agent2.currentReward!=20:
-    move1 = agent2.chooseMove(movelist,policy)
-    agent2.makeMove(move1, world)
-print(f"AgentTwo final position: {agent2.position}")
+agent1,agent2 = run()
 
 print(agent1.getPath())
 print(agent1.reward)
@@ -56,4 +75,17 @@ print(agent1.reward)
 print(agent2.getPath())
 print(agent2.reward)
 
-debugBoards(agent1,agent2)
+debugBoards(agent1)
+debugBoards(agent2)
+
+average1,average2 = many_runs(20)
+
+numbers = [average1, average2]
+labels = ['Random Agent', 'Greedy Agent']
+
+# Plot
+plt.bar(labels, numbers, color=['blue', 'orange'])
+plt.xlabel('Labels')
+plt.ylabel('Values')
+plt.title('Reward Bar Graph')
+plt.show()
