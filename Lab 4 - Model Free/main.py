cedc4c53bb042824eca092eb4f9132587936256b
Tumi Jourdan <2180153@students.wits.ac.za>
2024-08-31 20:11:45 +0200

    Submission Stuff

--- Lab 4 - Model Free/main.py
+++ Lab 4 - Model Free/main.py
@@ -1,34 +1,14 @@
+###
+# Group Members
+# Shakeel Malagas: 2424161 
+# Tumi Jourdan: 2180153
+# Dean Solomon: 2347848
+# Tao Yuan: 2332155
+###
+
 import gymnasium as gym
 import numpy as np
 import matplotlib.pyplot as plt
-import time
-# def one_step_lookahead(state, V,env):
-#     """
-#     Helper function to calculate the value for all action in a given state.
-#
-#     Args:
-#         state: The state to consider (int)
-#         V: The value to use as an estimator, Vector of length env.observation_space.n
-#
-#     Returns:
-#         A vector of length env.action_space.n containing the expected value of each action.
-#     """
-#
-#     # looking at state, we need to look at actions around said state, careful boundaries
-#     MAX_Y = env.shape[0]
-#     MAX_X = env.shape[1]
-#
-#     s = state
-#
-#     y = state // MAX_X
-#     x = state % MAX_X
-#
-#     ns_up = s if y == 0 else s - MAX_X
-#     ns_right = s if x == (MAX_X - 1) else s + 1
-#     ns_down = s if y == (MAX_Y - 1) else s + MAX_X
-#     ns_left = s if x == 0 else s - 1
-#     # up right down left
-#     return [V[ns_up],V[ns_right],V[ns_down],V[ns_left]]
 
 def eps_greedy(Q, state, eps):
     if np.random.random() < eps:
@@ -67,9 +47,7 @@ def sarsa(num_episodes, env: gym.Env, gamma, alpha, decay_rate, eps, save_folder
         if save_folder is not None:
             q_mean = np.mean(Q,axis=1)
             plt.imshow(np.reshape(q_mean,[4,12]))
-
             plt.colorbar() 
-
             plt.title( "2-D Heat Map" ) 
             plt.savefig(f"./{save_folder}/episode{episode}.png") 
             plt.close()
@@ -86,13 +64,9 @@ eps = 0.1
 decay_rate = 0
 
 # rewards = sarsa(200,env,gamma,alpha,decay_rate,eps, 'td0')
-
 # decay_rate = 0.3
-
 # rewards = sarsa(200,env,gamma,alpha,decay_rate,eps, 'td1')
-
 # decay_rate = 0.5
-
 # rewards = sarsa(200,env,gamma,alpha,decay_rate,eps, 'td2')
 
 td0_return = np.array([])
